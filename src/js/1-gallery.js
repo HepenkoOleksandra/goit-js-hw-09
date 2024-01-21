79c3266943e5f95a877d6f6c12e898af580a1878
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-01-21 16:06:50 +0200

    'update'

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -68,20 +68,6 @@ const galleryList = document.querySelector('.gallery');
 
 galleryList.addEventListener('click', onGalleryListClick);
 
-// function galleryTemplate({preview, original, description}) {
-//   return `
-//     <li class="gallery-item">
-//       <a class="gallery-link" href="${original}">
-//         <img
-//           class="gallery-image"
-//           src="${preview}"
-//           data-source="${original}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </li>`
-// }
-
 const markup = images.map(({ preview, original, description }) => {
     return `
     <li class="gallery-item">
